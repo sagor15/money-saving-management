7e5ebb6adf03d7cab4732853300834bfe4e3c302
sagor20202021 <sagorahammed1562@gamil.com>
2022-02-17 03:33:44 +0600

    javascript part to part commit

--- js/app.js
+++ js/app.js
@@ -24,10 +24,62 @@ document.getElementById('Calculate-btn').addEventListener('click',function(){
     const newBalance = document.getElementById('New-Balance');
     const newBalanceValue = newBalance.innerText;
     const resultNewBalance = parseFloat(incomeTotalValue)  - resultTotalCost;
-    newBalance.innerText = resultNewBalance;
+    const soreNewBalance = newBalance.innerText = resultNewBalance;
 
     FoodInput.value = '';
     RentInput.value= '';
     ClothInput.value = '';
     incomeTotal.value = '';
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById('save').addEventListener('click',function(){
+
+    
+
+
+    const incomeTotal = document.getElementById('Income-Input');
+    const incomeTotalValue = incomeTotal.value;
+// newBAlance 
+    const newBalance = document.getElementById('New-Balance');
+    const newBalanceValue = newBalance.innerText;
+    const resultNewBalance = parseFloat(incomeTotalValue)  - resultTotalCost;
+    const storeNewBalance = newBalance.innerText = resultNewBalance;
+
+// remaingBalance 
+    const remainingBalance = document.getElementById('remaining-balance');
+    const remainingBalanceValue = remainingBalance.innerText;
+
+    
+
+    const saveInput = document.getElementById('save-input');
+    const saveInputValue = saveInput.value;
+
+    const savingResult = parseFloat(incomeTotalValue) * parseFloat(saveInputValue) / 100;
+    console.log(savingResult);
+
+    const savingAmount = document.getElementById('saving-amount');
+    const savingAmountValue = savingAmount.innerText;
+    const resultsavingAmount = savingAmount.innerText = savingResult;
+
+
+    const resultremainingBalance = storeNewBalance - resultsavingAmount;
+    remainingBalance.innerText = resultremainingBalance;
+
+
+
+
+
 })
