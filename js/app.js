2d5606399306b7dbe9156d964b9609d093950857
sagor20202021 <sagorahammed1562@gamil.com>
2022-02-17 17:46:47 +0600

    funtion Reduce done

--- js/app.js
+++ js/app.js
@@ -1,85 +1,42 @@
 document.getElementById('Calculate-btn').addEventListener('click',function(){
-    // console.log('clicked');
-
-
-
-
-  
-    const FoodInput = document.getElementById('Food-Input');
-    const FoodValue = FoodInput.value;
-    const RentInput = document.getElementById('Rent-Input');
-    const RentValue = RentInput.value;
-    const ClothInput = document.getElementById('Cloth-Input');
-    const ClothValue = ClothInput.value;
-    const allItemPlus = parseFloat(FoodValue) + parseFloat(RentValue)+parseFloat(ClothValue);
+    //function call 
+    const allItemPlus = parseFloat(expense('Food')) + parseFloat(expense('Rent'))+parseFloat(expense('Cloth'));
     const TotalCost = document.getElementById('Total-cost');
-    const TotalCostValue = TotalCost.innerText;
     const resultTotalCost = TotalCost.innerText = allItemPlus;
-
-
+    // declare income-input
     const incomeTotal = document.getElementById('Income-Input');
     const incomeTotalValue = incomeTotal.value;
-
     // new balance 
     const newBalance = document.getElementById('New-Balance');
     const newBalanceValue = newBalance.innerText;
     const resultNewBalance = parseFloat(incomeTotalValue)  - resultTotalCost;
     const soreNewBalance = newBalance.innerText = resultNewBalance;
-
-    FoodInput.value = '';
-    RentInput.value= '';
-    ClothInput.value = '';
-    incomeTotal.value = '';
 })
 
-
-
-
-
-
-
-
-
-
-
-
+// function declare
+function expense(item){
+    const cost = document.getElementById(item + "-Input").value;
+    return cost;
+}
 
 
 document.getElementById('save').addEventListener('click',function(){
-
-    
-
-
     const incomeTotal = document.getElementById('Income-Input');
     const incomeTotalValue = incomeTotal.value;
 // newBAlance 
     const newBalance = document.getElementById('New-Balance');
     const newBalanceValue = newBalance.innerText;
-    const resultNewBalance = parseFloat(incomeTotalValue)  - resultTotalCost;
-    const storeNewBalance = newBalance.innerText = resultNewBalance;
-
+    const saveInput = document.getElementById('save-input');
+    const saveInputValue = saveInput.value;
+    const savingResult = parseFloat(incomeTotalValue) * (parseFloat(saveInputValue) / 100) ;
+// declare saving-amount 
+    const savingAmount = document.getElementById('saving-amount');
+    const savingAmountValue = savingAmount.innerText;
+    savingAmount.innerText = savingResult;
 // remaingBalance 
     const remainingBalance = document.getElementById('remaining-balance');
     const remainingBalanceValue = remainingBalance.innerText;
-
-    
-
-    const saveInput = document.getElementById('save-input');
-    const saveInputValue = saveInput.value;
-
-    const savingResult = parseFloat(incomeTotalValue) * parseFloat(saveInputValue) / 100;
-    console.log(savingResult);
-
-    const savingAmount = document.getElementById('saving-amount');
-    const savingAmountValue = savingAmount.innerText;
-    const resultsavingAmount = savingAmount.innerText = savingResult;
-
-
-    const resultremainingBalance = storeNewBalance - resultsavingAmount;
-    remainingBalance.innerText = resultremainingBalance;
-
-
-
-
-
+// result show reamaning er jaigai
+    const ReamaningAmountResult = parseFloat(newBalanceValue) - savingResult;
+    remainingBalance.innerText = ReamaningAmountResult;
 })
